e866c5a7a999273760858f0d7821bff0064f7a39
elynxd <eliandrodviana@gmail.com>
2025-09-04 18:57:33 -0300

    install and config biomejs

--- tasks/4-atividade/src/App.jsx
+++ tasks/4-atividade/src/App.jsx
@@ -1,21 +1,19 @@
-import './App.css';
-import { Heart } from 'phosphor-react';
+import "./App.css";
+import { Heart } from "phosphor-react";
 
 function App() {
-
-  return (
-    <>
-      <div className='flex justify-center items-center h-screen'>
-      <p className="text-3xl font-bold text-red-500">
-        Hello world!
-        <span className="inline-block ml-2 animate-pulse">
-           <Heart size={32} />
-        </span>
-      </p>
-      
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="flex h-screen items-center justify-center">
+                <p className="font-bold text-3xl text-red-500">
+                    Hello world!
+                    <span className="ml-2 inline-block animate-pulse">
+                        <Heart size={32} />
+                    </span>
+                </p>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
