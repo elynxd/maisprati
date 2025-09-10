40d9c05fd7c12ef78093d273d3c6db064108acac
elynxd <eliandrodviana@gmail.com>
2025-09-10 15:25:16 -0300

    another way to use a func handleClick to increase the count++

--- 3-ReactJS/3-Components/src/components/IncreaseFunc.jsx
+++ 3-ReactJS/3-Components/src/components/IncreaseFunc.jsx
@@ -3,10 +3,14 @@ import { useState } from "react"
 export const IncreaseFunc = () => {
     const [count, setCount] = useState(0);
 
+    function handleClick() {
+        setCount(count+1);
+    }
+
     return (
         <>
         <h1>Contador: {count}</h1>
-        <button onClick={() => setCount(count +1)}>Increase</button>
+        <button onClick={handleClick}>Increase</button>
     {count > 0 ? (
         <button onClick={() => setCount(count -1)}>Decrease</button>
     ) : (
